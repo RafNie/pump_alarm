e5765d83cf3a901ef7bd8c0d41d6a44110658c3e
Rafał Niedźwiedziński <rafal.niedzwiedzinski@o2.pl>
2022-02-09 21:46:16 +0100

    Code cleanup

--- main.c
+++ main.c
@@ -10,8 +10,50 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
+#include <assert.h>
 
-enum Alarm {
+// User definitions:
+//
+// seconds of current values below the threshold to turn on alarm
+#define seconds_to_alarm 20
+//(2*24*60*60)
+// seconds of current values over the threshold to clear current monitor counter
+#define seconds_of_current_measure 7
+// seconds without values of enable input above the threshold to disable current monitor counter
+#define seconds_of_inactivity_to_disable_alarm 10
+// (24*60*60)
+// threshold of enable input: ~ 1/8 Ucc=5V in ADC steps
+#define enable_input_analog_threshold 256
+// current transformer ratio in mV/A
+#define current_transformet_ratio 100
+// RMS current threshold in mA
+#define rms_current_alarm_threshold 3000L
+// End of user definitions
+
+// Pins definitions
+#define BUZZ (PB1)
+#define ALARM_RESET (PB0)
+#define CURRENT_TRANSFORMER_ADC (_BV(MUX0))
+#define ENABLE_MONITOR_ADC (_BV(MUX1))
+
+#if seconds_of_inactivity_to_disable_alarm >= seconds_to_alarm
+#error seconds_of_inactivity_to_disable_alarm should be less than seconds_to_alarm
+#endif
+
+// Don't edit. Alarm threshold calculation (fixed point) >>>>
+#define fixed_point 1000
+#define root_squere_of_two 1414
+#define max_current_threshold ((rms_current_alarm_threshold*root_squere_of_two)/fixed_point)
+#define voltage_threshold ((max_current_threshold*current_transformet_ratio)/fixed_point)
+#define adc_steps 1024
+#define adc_Vref 2560
+#define current_threshold ((adc_steps*voltage_threshold)/adc_Vref)
+// <<<<
+
+#define heart_beat_period 10
+#define samples_number 167
+
+enum States {
 	alarm,
 	start_current_measuring,
 	current_measuring,
@@ -20,69 +62,29 @@ enum Alarm {
 };
 
 enum Enable {
-	off,
-	on
+	current_monitoring_disabled,
+	current_monitoring_enabled
 };
 
-// Pins definitions
-#define BUZZ (PB1)
-//#define LED (PB1)
-#define ALARM_RESET (PB0)
-#define CURRENT_TRANSFORMER_ADC (_BV(MUX0))
-#define ENABLE_MONITOR_ADC (_BV(MUX1))
-
-//User definitions:
-//
-// seconds without current values above threshold to turn on alarm
-#define seconds_to_alarm 20
-//(2*24*60*60)
-// seconds without values of enable input above the threshold to disable alarm counter
-#define seconds_of_inactivity_to_disable_alarm 10
-// (24*60*60)
-// threshold enable input: ~ 1/8 Ucc=5V in ADC steps
-#define enable_monitor_analog_threshold 256
-// current transformer ratio in mV/A
-#define current_transformet_ratio 100
-// RMS current threshold in mA
-#define rms_current_alarm_threshold 3000L
-
-
-// Don't edit >>
-// Alarm threshold calculation (fixed point):
-#define fixed_point 1000
-#define root_squere_of_two 1414
-#define max_current_threshold ((rms_current_alarm_threshold*root_squere_of_two)/fixed_point)
-#define voltage_threshold ((max_current_threshold*current_transformet_ratio)/fixed_point)
-#define adc_steps 1024
-#define adc_Vref 2560
-#define alarm_threshold ((adc_steps*voltage_threshold)/adc_Vref)
-// <<
-
-#define measurements_above_threshold 7
-#define hart_beate_threshold 10
-#define samples_number 167
-
-
-enum Enable enableState = on; // switched by the ADC enable input
-volatile enum Alarm alarm_state = no_alarm;
+enum Enable enableState = current_monitoring_enabled; // switched by the ADC enable input
+volatile enum States state = no_alarm;
 volatile uint32_t currentMonitorCounter = seconds_to_alarm;
 volatile uint32_t enableMonitorCounter = seconds_of_inactivity_to_disable_alarm;
 volatile uint16_t currentMonitorBuffer[samples_number];
 volatile uint8_t currentMonitorBufferIndex = samples_number - 1;
-volatile uint8_t hartBeatCounter = hart_beate_threshold;
-uint8_t measurementsAboveThresholdCounter = measurements_above_threshold;
+volatile uint8_t heartBeatCounter = heart_beat_period;
+uint8_t measurementsAboveThresholdCounter = seconds_of_current_measure;
 
-void _delay_s(uint16_t sec);
 void handleCounters();
 void configureTimer();
 void configureIO();
 void precessAlarm();
 void precessResetAlarm();
 void procesEnableInput();
-void processCurrentMonitorInput();
+void processCurrentMeasurement();
 void checkCurrentValue();
 void checkAlarmConditions();
-void precessHartReate();
+void precessHeartBeat();
 void beep();
 
 int main(void) {
@@ -96,14 +98,14 @@ int main(void) {
 	configureTimer();
 
 	while (1) {
-		switch(alarm_state) {
+		switch(state) {
 		case alarm:
 			precessAlarm();
 			precessResetAlarm();
 			break;
 		case start_current_measuring:
 			procesEnableInput();
-			processCurrentMonitorInput();
+			processCurrentMeasurement();
 			break;
 		case current_measuring:
 			break;
@@ -113,7 +115,7 @@ int main(void) {
 		case no_alarm:
 			break;
 		}
-		precessHartReate();
+		precessHeartBeat();
 	};
 	return 0;
 }
@@ -125,16 +127,16 @@ void beep() {
 	PORTB &= ~_BV(BUZZ);
 }
 
-void precessHartReate() {
-	if (hartBeatCounter == 0) {
-		if (alarm_state != alarm)
+void precessHeartBeat() {
+	if (heartBeatCounter == 0) {
+		if (state != alarm)
 			beep();
-		hartBeatCounter = hart_beate_threshold;
+		heartBeatCounter = heart_beat_period;
 	}
 }
 
 void precessAlarm() {
-	if (hartBeatCounter%2) {
+	if (heartBeatCounter%2) {
 		PORTB ^= _BV(BUZZ);
 		_delay_us(180);
 	}
@@ -156,11 +158,11 @@ uint16_t findMinMaxDiff(volatile uint16_t* buf, uint32_t size) {
 
 void checkCurrentValue() {
 	uint16_t Ipp = findMinMaxDiff(currentMonitorBuffer, samples_number - 1);
-	if (Ipp/2 > alarm_threshold) {
-		measurementsAboveThresholdCounter--;
+	if (Ipp/2 > current_threshold) {
+		if (measurementsAboveThresholdCounter) measurementsAboveThresholdCounter--;
 	}
 	else {
-		measurementsAboveThresholdCounter = measurements_above_threshold;
+		measurementsAboveThresholdCounter = seconds_of_current_measure;
 	}
 	if (measurementsAboveThresholdCounter == 0) {
 		currentMonitorCounter = seconds_to_alarm;
@@ -170,16 +172,16 @@ void checkCurrentValue() {
 void checkAlarmConditions() {
 	checkCurrentValue();
 	if (currentMonitorCounter == 0) {
-		alarm_state = alarm;
+		state = alarm;
 	}
 	else {
-		alarm_state = no_alarm;
+		state = no_alarm;
 	}
 }
 
-void processCurrentMonitorInput() {
-	if (enableState == off) {
-		alarm_state = no_alarm;
+void processCurrentMeasurement() {
+	if (enableState == current_monitoring_disabled) {
+		state = no_alarm;
 		return;
 	}
 	ADMUX = CURRENT_TRANSFORMER_ADC;
@@ -187,10 +189,10 @@ void processCurrentMonitorInput() {
 	sei();
 	ADCSRA |= _BV(ADIE) | _BV(ADATE);// ADC interrupt enable, start in free running mode
 	ADCSRA |= _BV(ADSC);
-	alarm_state = current_measuring;
+	state = current_measuring;
 }
 
-uint16_t getEnableInputADC() {
+uint16_t getEnableInputADCValue() {
 	ADMUX = ENABLE_MONITOR_ADC;
 	_delay_ms(2);
 	ADCSRA |= _BV(ADSC); // Start conversion - first conversion may be not to accurate
@@ -201,13 +203,13 @@ uint16_t getEnableInputADC() {
 }
 
 void procesEnableInput() {
-	uint16_t enableVal = getEnableInputADC();
-	if (enableVal > enable_monitor_analog_threshold) {
+	uint16_t enableVal = getEnableInputADCValue();
+	if (enableVal > enable_input_analog_threshold) {
 		enableMonitorCounter = seconds_of_inactivity_to_disable_alarm;
-		enableState = on;
+		enableState = current_monitoring_enabled;
 	}
 	if (enableMonitorCounter == 0) {
-		enableState = off;
+		enableState = current_monitoring_disabled;
 	}
 }
 
@@ -221,7 +223,7 @@ void precessResetAlarm() {
 		if (isResetPinPressed()) {
 			currentMonitorCounter = seconds_to_alarm;
 			enableMonitorCounter = seconds_of_inactivity_to_disable_alarm;
-			alarm_state = no_alarm;
+			state = no_alarm;
 			PORTB &= ~_BV(BUZZ);
 			// or reset MCU
 		}
@@ -232,27 +234,16 @@ void configureIO() {
 	MCUCR &= ~_BV(PUD);
 
 	// BUZ: Dout
-	// LED: Dout
-	DDRB |= _BV(BUZZ);// | _BV(LED);
-	//PORTB |= _BV(LED);
+	DDRB |= _BV(BUZZ);
 
 	// ADC current transformer: PB2
 	// ADC enable monitor: PB4
 	ADMUX |= (_BV(REFS1) | _BV(REFS2)); //Uref 2,56V without C
 	ADCSRA |= (_BV(ADEN) | _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2)); // enable ADC and prescaler 128
 
-	// Alarm reset: Din pullup
+	// Alarm reset button: Din pullup
 	PORTB |= _BV(ALARM_RESET);
 	DDRB &= ~_BV(ALARM_RESET);
-}
-
-void _delay_s(uint16_t sec)
-{
-	sec *= 100;
-	for (uint16_t i=0; i<sec; i++)
-	{
-		_delay_ms(10);
-	}
 }
 
 void configureTimer() {
@@ -269,15 +260,15 @@ void configureTimer() {
 }
 
 inline void handleCounters() {
-	if (enableState == on && currentMonitorCounter) currentMonitorCounter--;
+	if (enableState == current_monitoring_enabled && currentMonitorCounter) currentMonitorCounter--;
 	if (enableMonitorCounter) enableMonitorCounter--;
-	if (hartBeatCounter) hartBeatCounter--;
+	if (heartBeatCounter) heartBeatCounter--;
 }
 
 ISR(TIMER1_COMPA_vect) {
 	handleCounters();
-	if (alarm_state == no_alarm)
-		alarm_state = start_current_measuring;
+	if (state == no_alarm)
+		state = start_current_measuring;
 }
 
 ISR(ADC_vect) {
@@ -285,7 +276,7 @@ ISR(ADC_vect) {
 	if (currentMonitorBufferIndex == 0) {
 		ADCSRA &= ~(_BV(ADIE) | _BV(ADATE)); //stop ADC
 		currentMonitorBufferIndex = samples_number - 1;
-		alarm_state = checking_alarm_conditions;
+		state = checking_alarm_conditions;
 	}
 }
 
