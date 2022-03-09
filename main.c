191b3adf72f64ba96b870bb3924dc26daf9e7cfc
Rafał Niedźwiedziński <rafal.niedzwiedzinski@o2.pl>
2022-03-09 17:31:04 +0100

    Signalisation of RMS current by beeps

--- main.c
+++ main.c
@@ -14,18 +14,19 @@
 
 // User definitions:
 //
-// seconds of current values below the threshold to turn on alarm
-#define seconds_to_alarm (2*24*60*60)
-// seconds of current values over the threshold to clear current monitor counter
+// Time to activate alarm when the current values are below the threshold [seconds]
+#define seconds_to_alarm (2L*24*60*60)
+// Time to clear current monitor counter by the current values over the threshold [seconds]
 #define seconds_of_current_measure 4
-// seconds without values of enable input above the threshold to disable current monitor counter
-#define seconds_of_inactivity_to_disable_alarm (24*60*60)
+// Time to disable current monitor counter without values of the enable input above the threshold [seconds]
+#define seconds_of_inactivity_to_disable_alarm (24L*60*60)
 // threshold of enable input: ~ 1/8 Ucc=5V in ADC steps
 #define enable_input_analog_threshold 256
 // current transformer ratio in mV/A
-#define current_transformet_ratio 90
+#define current_transformet_ratio 98
 // RMS current threshold in mA
-#define rms_current_alarm_threshold 3000L
+#define rms_current_alarm_threshold 3000
+//
 // End of user definitions
 
 // Pins definitions
@@ -38,40 +39,57 @@
 #error seconds_of_inactivity_to_disable_alarm should be less than seconds_to_alarm
 #endif
 
-// Don't edit. Alarm threshold calculation (fixed point) >>>>
-#define fixed_point 1000
-#define root_squere_of_two 1414
-#define max_current_threshold ((rms_current_alarm_threshold*root_squere_of_two)/fixed_point)
-#define voltage_threshold ((max_current_threshold*current_transformet_ratio)/fixed_point)
+// ADC constants
+#define fixed_point_precision 1000
+#define root_squere_of_two 1414L
 #define adc_steps 1024
 #define adc_Vref 2560
+// Don't edit, calculations:
+// Alarm threshold calculation (fixed point)
+// >>>>
+#define max_current_threshold ((rms_current_alarm_threshold*root_squere_of_two)/fixed_point_precision)
+#define voltage_threshold ((max_current_threshold*current_transformet_ratio)/fixed_point_precision)
 #define current_threshold ((adc_steps*voltage_threshold)/adc_Vref)
 // <<<<
+// ADC steps per 1A RMS
+// >>>>
+#define mV_per_A (((1*root_squere_of_two)*current_transformet_ratio)/fixed_point_precision)
+#define steps_per_ampere ((adc_steps*85L)/adc_Vref)
+// <<<<
 
-#define heart_beat_period 10
-#define samples_number 167
+#define heart_beat_period 20
+#define samples_number 198
 
-enum States {
+typedef enum {
 	alarm,
 	start_current_measuring,
 	current_measuring,
 	checking_alarm_conditions,
 	no_alarm
-};
+} States;
 
-enum Enable {
+typedef enum {
 	current_monitoring_disabled,
 	current_monitoring_enabled
-};
+} CurrentMonitoring;
 
-enum Enable enableState = current_monitoring_enabled; // switched by the ADC enable input
-volatile enum States state = no_alarm;
+typedef enum {
+	false = 0,
+	true = 1
+} Bool;
+
+CurrentMonitoring enableState = current_monitoring_enabled; // switched by the ADC enable input
+volatile States state = no_alarm;
 volatile uint32_t currentMonitorCounter = seconds_to_alarm;
 volatile uint32_t enableMonitorCounter = seconds_of_inactivity_to_disable_alarm;
 volatile uint16_t currentMonitorBuffer[samples_number];
-volatile uint8_t currentMonitorBufferIndex = samples_number - 1;
+volatile uint8_t currentMonitorBufferIndex = samples_number;
 volatile uint8_t heartBeatCounter = heart_beat_period;
 uint8_t measurementsAboveThresholdCounter = seconds_of_current_measure;
+Bool currentPresentationFeature = false;
+
+void checkeEnableOfCurrentPresentationFeatureAtStartup();
+void presentCurrentLevelIfNeed(uint16_t currentValue);
 
 void handleCounters();
 void configureTimer();
@@ -88,11 +106,19 @@ void beep();
 int main(void) {
 
 	CLKPR = _BV(CLKPCE);
-	CLKPR = _BV(CLKPS2);// prescaler 16
+	CLKPR = _BV(CLKPS2);// prescaler 16, for 1MHz FCPU
 
 	configureIO();
+	checkeEnableOfCurrentPresentationFeatureAtStartup();
 	//enable watchdog
-	beep(); _delay_ms(100); beep();
+
+	beep();
+	_delay_ms(100);
+	beep();
+	if (currentPresentationFeature) {
+		_delay_ms(100); beep();
+	}
+
 	configureTimer();
 
 	while (1) {
@@ -118,8 +144,30 @@ int main(void) {
 	return 0;
 }
 
+inline uint8_t isResetPinPressed() {
+	return (PINB & _BV(ALARM_RESET)) ? 0 : 1;
+}
+
+void checkeEnableOfCurrentPresentationFeatureAtStartup() {
+	if (isResetPinPressed()) {
+			_delay_ms(300);
+			if (isResetPinPressed()) {
+				currentPresentationFeature = true;
+			}
+		}
+}
+
+void presentCurrentLevelIfNeed(uint16_t currentValue) {
+	if (currentPresentationFeature) {
+		uint8_t ampere = currentValue/steps_per_ampere;
+		for (uint8_t i=0; i<ampere; i++) {
+			beep(); _delay_ms(100);
+		}
+	}
+}
+
 void beep() {
-	for (uint16_t i = 0; i<70; i++) {
+	for (uint16_t i = 0; i<50; i++) {
 		PORTB ^= _BV(BUZZ); _delay_us(150);
 	}
 	PORTB &= ~_BV(BUZZ);
@@ -146,7 +194,7 @@ void precessAlarm() {
 uint16_t findMinMaxDiff(volatile uint16_t* buf, uint32_t size) {
 	uint16_t min = *buf, max = *buf;
 	if (size > 0) {
-		for (int i=1; i<size; i++) {
+		for (int i=0; i<size; i++) {
 			if (buf[i] > max) max = buf[i];
 			if (buf[i] < min) min = buf[i];
 		}
@@ -155,8 +203,10 @@ uint16_t findMinMaxDiff(volatile uint16_t* buf, uint32_t size) {
 }
 
 void checkCurrentValue() {
-	uint16_t Ipp = findMinMaxDiff(currentMonitorBuffer, samples_number - 1);
-	if (Ipp/2 > current_threshold) {
+	uint16_t Ipp = findMinMaxDiff(currentMonitorBuffer, samples_number);
+	uint16_t I = Ipp/2;
+	presentCurrentLevelIfNeed(I);
+	if (I > current_threshold) {
 		if (measurementsAboveThresholdCounter) measurementsAboveThresholdCounter--;
 	}
 	else {
@@ -184,6 +234,7 @@ void processCurrentMeasurement() {
 	}
 	ADMUX = CURRENT_TRANSFORMER_ADC;
 	_delay_ms(2);
+
 	sei();
 	ADCSRA |= _BV(ADIE) | _BV(ADATE);// ADC interrupt enable, start in free running mode
 	ADCSRA |= _BV(ADSC);
@@ -211,10 +262,6 @@ void procesEnableInput() {
 	}
 }
 
-inline uint8_t isResetPinPressed() {
-	return (PINB & _BV(ALARM_RESET)) ? 0 : 1;
-}
-
 void precessResetAlarm() {
 	if (isResetPinPressed()) {
 		_delay_ms(30);
@@ -237,7 +284,8 @@ void configureIO() {
 	// ADC current transformer: PB2
 	// ADC enable monitor: PB4
 	ADMUX |= (_BV(REFS1) | _BV(REFS2)); //Uref 2,56V without C
-	ADCSRA |= (_BV(ADEN) | _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2)); // enable ADC and prescaler 128
+	ADCSRA |= (_BV(ADEN) | _BV(ADPS0)) | _BV(ADPS1)); // enable ADC and prescaler 8
+	// conversion time 13,5 ADCclk cycles
 
 	// Alarm reset button: Din pullup
 	PORTB |= _BV(ALARM_RESET);
@@ -270,10 +318,10 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 ISR(ADC_vect) {
-	currentMonitorBuffer[currentMonitorBufferIndex--] = ADC;
+	currentMonitorBuffer[--currentMonitorBufferIndex] = ADC;
 	if (currentMonitorBufferIndex == 0) {
 		ADCSRA &= ~(_BV(ADIE) | _BV(ADATE)); //stop ADC
-		currentMonitorBufferIndex = samples_number - 1;
+		currentMonitorBufferIndex = samples_number;
 		state = checking_alarm_conditions;
 	}
 }
